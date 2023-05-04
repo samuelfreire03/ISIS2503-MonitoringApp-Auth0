0a36addb5bc22090ef7523d6259dc636b9aab6df
samuelfreire03 <s.freire@uniandes.edu.co>
2023-05-04 16:21:09 -0500

    cambios bobos

--- measurements/models.py
+++ measurements/models.py
@@ -10,4 +10,4 @@ class Measurement(models.Model):
     enfermedad = models.CharField(max_length=50, default=None)
 
     def __str__(self):
-        return '%s %s' % (self.fechaConsulta, self.lugarConsulta)s
+        return '%s %s' % (self.fechaConsulta, self.lugarConsulta)
